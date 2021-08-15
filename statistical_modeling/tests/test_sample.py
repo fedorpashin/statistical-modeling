fa4f0fbf1503b431a569e7191ace396189582e9a
Fedor Pashin <fedorpashin@icloud.com>
2021-08-15 17:56:45 +0300

    #4 Fix naming

--- statistical_modeling/tests/test_sample.py
+++ statistical_modeling/tests/test_sample.py
@@ -8,9 +8,9 @@ class TestSample(unittest.TestCase):
     def test_quantities(self):
         s = Sample([458, -184, 59, 325, -530])
         with self.subTest():
-            assert_allclose(s.E, 25.6)
+            assert_allclose(s.mean, 25.6)
         with self.subTest():
-            assert_allclose(s.D, 126069.84)
+            assert_allclose(s.variance, 126069.84)
 
 
 if __name__ == '__main__':
